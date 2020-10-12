9632d96c79e2819c3ee80779a3d4f85e5e5a4857
TUXEDO FALCON <alerod0204@gmail.com>
2020-10-12 20:57:37 +0000

    Added variables and colors.

--- index.java
+++ index.java
@@ -16,10 +16,21 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
+int rectX = 30;
+int rectY = 30;
+
+int circleX = 130;
+int circleY = 130;
+
   background(255);
   stroke(0);
+  fill(0);
+  
+  rect(rectX, rectY, 200, 200);
 
+fill(255, 255, 0);
 
+  ellipse(circleX, circleY, 80, 80);
   
 
 }
